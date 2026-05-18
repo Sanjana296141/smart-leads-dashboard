441f6da635ab879f3e11c3af825d31d848fb57fc
Sanjana <girisanjana2004@gmail.com>
2026-05-19 03:02:21 +0530

    Updated frontend API for production

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-leads-backend-n3sz.onrender.com",
 });
 
 export default API;
