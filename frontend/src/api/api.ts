1052f88681c1223f22c556da9ebfb2ae8822c058
Sanjana <girisanjana2004@gmail.com>
2026-05-19 04:41:52 +0530

    Fixed API base URL

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smart-leads-backend-n3sz.onrender.com",
+  baseURL:
+    "https://smart-leads-backend-n3sz.onrender.com/api",
 });
 
 export default API;
